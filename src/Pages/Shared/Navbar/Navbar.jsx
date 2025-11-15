ac1891ea71cf76c905489b989a80a942ad193569
ehasun15019 <ehasunulislam0202@gmail.com>
2025-11-16 00:47:16 +0600

    setup Navbar & Footer

---
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import Logo from "../../../Components/Logo/Logo";
+import { FaBars } from "react-icons/fa";
+import { NavLink } from "react-router";
+
+const Navbar = () => {
+  const links = (
+    <>
+      <li>
+        <NavLink>Ser</NavLink>
+      </li>
+      <li>
+        <NavLink>Ser</NavLink>
+      </li>
+    </>
+  );
+
+  return (
+    <div className="navbar bg-base-100 shadow-sm">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <FaBars />
+          </div>
+          <ul
+            tabIndex="-1"
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+          >
+            {links}
+          </ul>
+        </div>
+        <a className="btn btn-ghost text-xl">
+          <Logo></Logo>
+        </a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          {links}
+        </ul>
+      </div>
+      <div className="navbar-end">
+        <a className="btn">Button</a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
